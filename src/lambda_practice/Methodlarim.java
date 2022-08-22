3ae46c71019fbce6bb082b2a595576566458c4cf
muzafferaydintechproed <muzafferaydin.techproed@gmail.com>
2022-08-22 18:44:30 +0300

    22/08/2022

--- src/lambda_practice/Methodlarim.java
+++ src/lambda_practice/Methodlarim.java
@@ -1,2 +1,18 @@
-package lambda_practice;public class Methodlarim {
+package lambda_practice;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class Methodlarim {//seed (tohum) methodlar create edildi
+
+
+    public static boolean ciftElemaniBul(int x){
+        return  x % 2==0;
+      // 2.yol return x % 2!=1;
+    }
+
+
+
+
+
 }
